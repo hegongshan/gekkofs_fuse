a33d3fed21c67596b631888ee06314b69560b5b3
Marc Vef <vef@uni-mainz.de>
2017-03-17 16:46:07 +0100

    Directory fuse ops function header fix

--- fs/src/fuse_ops/directory.cpp
+++ fs/src/fuse_ops/directory.cpp
@@ -5,11 +5,15 @@
 #include "../main.h"
 #include "../fuse_ops.h"
 
-int adafs_opendir(const char*, struct fuse_file_info*) {
+int adafs_opendir(const char* p, struct fuse_file_info* fi) {
     return 0;
 }
 
-int adafs_readdir(const char*, void*, fuse_fill_dir_t, off_t, struct fuse_file_info*, enum fuse_readdir_flags) {
+int adafs_readdir(const char* p, void* buf, fuse_fill_dir_t filler, off_t offset,
+                  struct fuse_file_info* fi, enum fuse_readdir_flags flags) {
+    ADAFS_DATA->logger->info("bb_readdir(path=\"{}\", buf={}, offset={}", p, buf, offset);
+//    ADAFS_DATA->logger->info("bb_readdir(path=\"%s\", buf=0x%08x, filler=0x%08x, offset=%lld, fi=0x%08x)",
+//                             p, buf, filler, offset, fi);
     return 0;
 }
 
