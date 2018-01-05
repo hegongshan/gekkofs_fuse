cb3eb3d5e986058b4279d26ec65642cde91a3975
Marc Vef <vef@uni-mainz.de>
2018-01-05 15:45:04 +0100

    ifs_test: MPI test is now given the total number of files distributed across all ranks

--- ifs_test/main_MPI.cpp
+++ ifs_test/main_MPI.cpp
@@ -11,7 +11,8 @@ using get_time = chrono::steady_clock;
 
 int main(int argc, char* argv[]) {
 
-    auto filen = atoi(argv[1]);
+//    auto filen = atoi(argv[1]);
+    auto total_files = atoi(argv[1]);
 
     MPI_Init(NULL, NULL);
     int world_size;
@@ -19,7 +20,8 @@ int main(int argc, char* argv[]) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    auto total_files = filen * world_size;
+//    auto total_files = filen * world_size;
+    auto filen = total_files / world_size;
 
 //    int filen = 3;
 
