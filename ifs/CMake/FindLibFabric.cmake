43f3a79e84498fe5b04bb8cd61dffeafee1d5f1c
Marc Vef <vef@uni-mainz.de>
2018-03-25 11:13:46 +0200

    CMake: Be more conservative when searching for LibFabric
    Previously, we gave already installed libfabric versions no chance
    to be detected over exclusively installed libfabric

--- ifs/CMake/FindLibFabric.cmake
+++ ifs/CMake/FindLibFabric.cmake
@@ -9,26 +9,26 @@ find_path(LIBFABRIC_DIR
 
 find_path(LIBFABRIC_INCLUDE_DIR rdma/fabric.h
         HINTS
-        ${ADAFS_DEPS_INSTALL}
         ${LIBFABRIC_DIR}
         $ENV{HOME}/opt
         /usr
         /usr/local
         /usr/local/adafs
         /opt
+    ${ADAFS_DEPS_INSTALL}
         PATH_SUFFIXES include
         PATH_SUFFIXES include/libfabric
         )
 
 find_library(LIBFABRIC_LIBRARY NAMES fabric
         HINTS
-        ${ADAFS_DEPS_INSTALL}
         ${LIBFABRIC_DIR}
         $ENV{HOME}/opt
         /usr
         /usr/local
         /usr/local/adafs
         /opt/
+    ${ADAFS_DEPS_INSTALL}
         PATH_SUFFIXES lib
         PATH_SUFFIXES lib/libfabric
         )
