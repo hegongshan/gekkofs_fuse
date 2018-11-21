66764e5a83c4b059de229edc2832d844d726f154
Tommaso Tocci <tommaso@tocci.pro>
2018-11-21 22:13:59 +0100

    Cleanup config rpc

--- ifs/src/preload/rpc/ld_rpc_management.cpp
+++ ifs/src/preload/rpc/ld_rpc_management.cpp
@@ -77,52 +77,56 @@ bool ipc_send_get_fs_config() {
             break;
         }
     }
-    if (send_ret == HG_SUCCESS) {
-        /* decode response */
-        CTX->log()->debug("{}() Waiting for response", __func__);
-        ret = margo_get_output(handle, &out);
-        if (ret == HG_SUCCESS) {
-            if (!CTX->mountdir().empty() && CTX->mountdir() != out.mountdir) {
-                CTX->log()->warn(
-                        "{}() fs_conf mountdir {} and received out.mountdir {} mismatch detected! Using received mountdir",
-                        __func__, CTX->mountdir(), out.mountdir);
-                CTX->mountdir(out.mountdir);
-            }
-            CTX->fs_conf()->rootdir = out.rootdir;
-            CTX->fs_conf()->atime_state = out.atime_state;
-            CTX->fs_conf()->mtime_state = out.mtime_state;
-            CTX->fs_conf()->ctime_state = out.ctime_state;
-            CTX->fs_conf()->uid_state = out.uid_state;
-            CTX->fs_conf()->gid_state = out.gid_state;
-            CTX->fs_conf()->link_cnt_state = out.link_cnt_state;
-            CTX->fs_conf()->blocks_state = out.blocks_state;
-            CTX->fs_conf()->uid = out.uid;
-            CTX->fs_conf()->gid = out.gid;
-            CTX->fs_conf()->host_id = out.host_id;
-            CTX->fs_conf()->host_size = out.host_size;
-            CTX->fs_conf()->rpc_port = std::to_string(RPC_PORT);
-
-            // split comma separated host string and create a hosts map
-            std::string hosts_raw = out.hosts_raw;
-            std::map<uint64_t, std::string> hostmap;
-            boost::char_separator<char> sep(",");
-            boost::tokenizer<boost::char_separator<char>> tok(hosts_raw, sep);
-            uint64_t i = 0;
-            for (auto&& s : tok) {
-                hostmap[i++] = s;
-            }
-            CTX->fs_conf()->hosts = hostmap;
-            CTX->log()->debug("{}() Got response with mountdir {}", __func__, out.mountdir);
-        } else {
-            CTX->log()->error("{}() Retrieving fs configurations from daemon", __func__);
-        }
-        /* clean up resources consumed by this rpc */
-        margo_addr_free(ld_margo_rpc_id, local_addr);
-        margo_free_output(handle, &out);
-    } else {
+    if (send_ret != HG_SUCCESS) {
         CTX->log()->warn("{}() timed out", __func__);
+        margo_destroy(handle);
+        return false;
     }
 
+    /* decode response */
+    CTX->log()->debug("{}() Waiting for response", __func__);
+    ret = margo_get_output(handle, &out);
+    if (ret != HG_SUCCESS) {
+        CTX->log()->error("{}() Retrieving fs configurations from daemon", __func__);
+        margo_destroy(handle);
+        return false;
+    }
+
+    if (!CTX->mountdir().empty() && CTX->mountdir() != out.mountdir) {
+        CTX->log()->warn(
+                "{}() fs_conf mountdir {} and received out.mountdir {} mismatch detected! Using received mountdir",
+                __func__, CTX->mountdir(), out.mountdir);
+        CTX->mountdir(out.mountdir);
+    }
+    CTX->fs_conf()->rootdir = out.rootdir;
+    CTX->fs_conf()->atime_state = out.atime_state;
+    CTX->fs_conf()->mtime_state = out.mtime_state;
+    CTX->fs_conf()->ctime_state = out.ctime_state;
+    CTX->fs_conf()->uid_state = out.uid_state;
+    CTX->fs_conf()->gid_state = out.gid_state;
+    CTX->fs_conf()->link_cnt_state = out.link_cnt_state;
+    CTX->fs_conf()->blocks_state = out.blocks_state;
+    CTX->fs_conf()->uid = out.uid;
+    CTX->fs_conf()->gid = out.gid;
+    CTX->fs_conf()->host_id = out.host_id;
+    CTX->fs_conf()->host_size = out.host_size;
+    CTX->fs_conf()->rpc_port = std::to_string(RPC_PORT);
+
+    // split comma separated host string and create a hosts map
+    std::string hosts_raw = out.hosts_raw;
+    std::map<uint64_t, std::string> hostmap;
+    boost::char_separator<char> sep(",");
+    boost::tokenizer<boost::char_separator<char>> tok(hosts_raw, sep);
+    uint64_t i = 0;
+    for (auto&& s : tok) {
+        hostmap[i++] = s;
+    }
+    CTX->fs_conf()->hosts = hostmap;
+    CTX->log()->debug("{}() Got response with mountdir {}", __func__, out.mountdir);
+
+    /* clean up resources consumed by this rpc */
+    margo_addr_free(ld_margo_rpc_id, local_addr);
+    margo_free_output(handle, &out);
     margo_destroy(handle);
-    return ret == HG_SUCCESS;
+    return true;
 }
