jeanbez/gekko-fwd

syscall_intercept

ifs/src/preload/intercept.cpp
@@ -1,0 +1,97 @@
+#include "preload/intercept.hpp"
+#include "preload/preload.hpp"
+#include "preload/hooks.hpp"
+
+#ifndef NDEBUG
+#include "preload/syscall_names.hpp"
+#endif
+
+#include <libsyscall_intercept_hook_point.h>
+#include <syscall.h>
+#include <errno.h>
+
+
+#define NOT_HOOKED 1
+#define HOOKED 0
+
+
+static inline int hook(long syscall_number,
+         long arg0, long arg1, long arg2,
+         long arg3, long arg4, long arg5,
+         long *result)
+{
+
+    switch (syscall_number) {
+
+    case SYS_open:
+        *result = hook_openat(AT_FDCWD,
+                              reinterpret_cast<char*>(arg0),
+                              static_cast<int>(arg1),
+                              static_cast<mode_t>(arg2));
+        break;
+
+    default:
+        /*
+         * Ignore any other syscalls
+         * i.e.: pass them on to the kernel
+         * as would normally happen.
+         */
+
+        #ifndef NDEBUG
+        CTX->log()->trace("Syscall [{}, {}]  Passthrough", syscall_names[syscall_number], syscall_number);
+        #endif
+        return NOT_HOOKED;
+    }
+
+    #ifndef NDEBUG
+    CTX->log()->trace("Syscall [{}, {}]  Intercepted", syscall_names[syscall_number], syscall_number);
+    #endif
+    return HOOKED;
+}
+
+
+static __thread bool guard_flag;
+
+int
+hook_guard_wrapper(long syscall_number,
+                   long arg0, long arg1, long arg2,
+                   long arg3, long arg4, long arg5,
+                   long *syscall_return_value)
+{
+    assert(CTX->initialized());
+
+    if (guard_flag) {
+        return NOT_HOOKED;
+    }
+
+    int is_hooked;
+
+    guard_flag = true;
+    int oerrno = errno;
+    is_hooked = hook(syscall_number,
+                     arg0, arg1, arg2, arg3, arg4, arg5,
+                     syscall_return_value);
+    errno = oerrno;
+    guard_flag = false;
+
+    return is_hooked;
+}
+
+
+void start_interception() {
+    assert(CTX->initialized());
+#ifndef NDEBUG
+    CTX->log()->debug("Activating interception of syscalls");
+#endif
+    // Set up the callback function pointer
+    intercept_hook_point = hook_guard_wrapper;
+}
+
+void stop_interception() {
+    assert(CTX->initialized());
+#ifndef NDEBUG
+    CTX->log()->debug("Deactivating interception of syscalls");
+#endif
+    // Reset callback function pointer
+    intercept_hook_point = nullptr;
+}

ifs/src/preload/preload.cpp
@@ -8,8 +8,8 @@
 #include <global/rpc/distributor.hpp>
 #include <preload/margo_ipc.hpp>
 #include <preload/rpc/ld_rpc_data_ws.hpp>
-#include <preload/passthrough.hpp>
 #include <preload/preload_util.hpp>
+#include <preload/intercept.hpp>
 
 #include <fstream>
 
@@ -322,7 +322,6 @@ void log_prog_name() {
  * Called initially ONCE when preload library is used with the LD_PRELOAD environment variable
  */
 void init_preload() {
-    init_passthrough_if_needed();
     init_logging();
     CTX->log()->debug("Initialized logging subsystem");
     log_prog_name();
@@ -335,14 +334,17 @@ void init_preload() {
     } else {
         CTX->log()->info("{}() mountdir '{}' loaded", __func__, CTX->mountdir());
     }
+    init_ld_env_if_needed();
     CTX->initialized(true);
     CTX->log()->debug("{}() exit", __func__);
+    start_interception();
 }
 
 /**
  * Called last when preload library is used with the LD_PRELOAD environment variable
  */
 void destroy_preload() {
+    stop_interception();
     auto services_used = (ld_margo_ipc_id != nullptr || ld_margo_rpc_id != nullptr);
 #ifdef MARGODIAG
     if (ld_margo_ipc_id != nullptr) {

hegongshan/gekkofs_fuse

src/client/intercept.cpp
@@ -1,0 +1,107 @@
+/*
+  Copyright 2018-2019, Barcelona Supercomputing Center (BSC), Spain
+  Copyright 2015-2019, Johannes Gutenberg Universitaet Mainz, Germany
+
+  This software was partially supported by the
+  EC H2020 funded project NEXTGenIO (Project ID: 671951, www.nextgenio.eu).
+
+  This software was partially supported by the
+  ADA-FS project under the SPPEXA project funded by the DFG.
+
+  SPDX-License-Identifier: MIT
+*/
+
+#include "client/intercept.hpp"
+#include "client/preload.hpp"
+#include "client/hooks.hpp"
+
+#ifndef NDEBUG
+#include "client/syscall_names.hpp"
+#endif
+
+#include <libsyscall_intercept_hook_point.h>
+#include <syscall.h>
+#include <errno.h>
+
+
+#define NOT_HOOKED 1
+#define HOOKED 0
+
+
+static inline int hook(long syscall_number,
+         long arg0, long arg1, long arg2,
+         long arg3, long arg4, long arg5,
+         long *result)
+{
+
+    switch (syscall_number) {
+
+    case SYS_open:
+        *result = hook_openat(AT_FDCWD,
+                              reinterpret_cast<char*>(arg0),
+                              static_cast<int>(arg1),
+                              static_cast<mode_t>(arg2));
+        break;
+
+    default:
+        /*
+         * Ignore any other syscalls
+         * i.e.: pass them on to the kernel
+         * as would normally happen.
+         */
+
+        #ifndef NDEBUG
+        CTX->log()->trace("Syscall [{}, {}]  Passthrough", syscall_names[syscall_number], syscall_number);
+        #endif
+        return NOT_HOOKED;
+    }
+
+    #ifndef NDEBUG
+    CTX->log()->trace("Syscall [{}, {}]  Intercepted", syscall_names[syscall_number], syscall_number);
+    #endif
+    return HOOKED;
+}
+
+
+static __thread bool guard_flag;
+
+int
+hook_guard_wrapper(long syscall_number,
+                   long arg0, long arg1, long arg2,
+                   long arg3, long arg4, long arg5,
+                   long *syscall_return_value)
+{
+    assert(CTX->interception_enabled());
+
+    if (guard_flag) {
+        return NOT_HOOKED;
+    }
+
+    int is_hooked;
+
+    guard_flag = true;
+    int oerrno = errno;
+    is_hooked = hook(syscall_number,
+                     arg0, arg1, arg2, arg3, arg4, arg5,
+                     syscall_return_value);
+    errno = oerrno;
+    guard_flag = false;
+
+    return is_hooked;
+}
+
+
+void start_interception() {
+    assert(CTX->interception_enabled());
+#ifndef NDEBUG
+    CTX->log()->debug("Activating interception of syscalls");
+#endif
+    // Set up the callback function pointer
+    intercept_hook_point = hook_guard_wrapper;
+}
+
+void stop_interception() {
+    assert(CTX->interception_enabled());
+    // Reset callback function pointer
+    intercept_hook_point = nullptr;
+}

src/client/preload.cpp
@@ -20,8 +20,8 @@
 #include <global/rpc/distributor.hpp>
 #include "global/rpc/rpc_types.hpp"
 #include <client/rpc/ld_rpc_management.hpp>
-#include <client/passthrough.hpp>
 #include <client/preload_util.hpp>
+#include <client/intercept.hpp>
 
 #include <fstream>
 
@@ -224,7 +224,6 @@ void log_prog_name() {
  * Called initially ONCE when preload library is used with the LD_PRELOAD environment variable
  */
 void init_preload() {
-    init_passthrough_if_needed();
     init_logging();
     CTX->log()->debug("Initialized logging subsystem");
     log_prog_name();
@@ -237,14 +236,17 @@ void init_preload() {
     } else {
         CTX->log()->info("{}() mountdir '{}' loaded", __func__, CTX->mountdir());
     }
+    init_ld_env_if_needed();
     CTX->enable_interception();
     CTX->log()->debug("{}() exit", __func__);
+    start_interception();
 }
 
 /**
  * Called last when preload library is used with the LD_PRELOAD environment variable
  */
 void destroy_preload() {
+    stop_interception();
     CTX->disable_interception();
     if (ld_margo_rpc_id == nullptr) {
         CTX->log()->debug("{}() No services in preload library used. Nothing to shut down.", __func__);