4cdc6b73b0ad71af0ec63c9171506d171755b4cf
Tommaso Tocci <tommaso@tocci.pro>
2019-08-09 12:46:15 +0200

    deps: update Mercury

--- scripts/dl_dep.sh
+++ scripts/dl_dep.sh
@@ -189,7 +189,7 @@ if [ "${NA_LAYER}" == "ofi" ] || [ "${NA_LAYER}" == "all" ]; then
     fi
 fi
 # get Mercury
-clonedeps "mercury" "https://github.com/mercury-hpc/mercury" "9906f25b6f9c52079d57006f199b3ea47960c435"  "--recurse-submodules" &
+clonedeps "mercury" "https://github.com/mercury-hpc/mercury" "cfbf9498fc550a7ba04aa10e93fb8e0e2b9aecbc"  "--recurse-submodules" &
 # get Argobots
 wgetdeps "argobots" "https://github.com/pmodels/argobots/archive/v1.0rc1.tar.gz" &
 # get Margo
