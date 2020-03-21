35f2f7e68e5560941790870436ec427de19ee0cd
Alberto Miranda <alberto.miranda@bsc.es>
2020-03-21 12:39:50 +0100

    Add timeout to test_cp

--- tests/shell/test_cp.py
+++ tests/shell/test_cp.py
@@ -22,7 +22,7 @@ def test_cp(gkfs_daemon, gkfs_shell, file_factory):
     lf01 = file_factory.create(file01, size=4.0, unit='MB')
 
     logger.info("copying into gkfs")
-    cmd = gkfs_shell.cp(lf01.pathname, gkfs_daemon.mountdir)
+    cmd = gkfs_shell.cp(lf01.pathname, gkfs_daemon.mountdir, timeout=60)
     assert cmd.exit_code == 0
     assert cmd.stdout.decode() == ''
     assert cmd.stderr.decode() == ''
